d44d976967b36a6c6b758b2697e107c27ce9da34
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-01-09 16:31:10 +0530

    Update largeno.c

--- largeno.c
+++ largeno.c
@@ -3,10 +3,10 @@ int main()
 {
 int a,b,c;
 printf("Enter three numbers");
-scanf("%d %d %d",&a.&b,&c);
+scanf("%d %d %d",&a,&b,&c);
 if(a>b && a>c)
 printf("%d is largest number",a);
-else if(b<c)
+else if(b>c)
 printf("%d is largest number",b);
 else
 printf("%d is largest number",c);
