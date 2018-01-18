ec56ef5e6989d9c07fdadf653995554ca4dbf721
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-01-18 16:33:24 +0530

    Create 3set3.c

---
+++ 3set3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,i,c=0;
+printf("Enter a number");
+scanf("%d",&n);
+for(i=2;i<n;i++)
+{
+if(n%i==0)
+{
+c++;
+}
+}
+if(c==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
