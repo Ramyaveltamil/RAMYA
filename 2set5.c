7faea8e2bd13a5bccbee8276468a93e2c040d99f
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-01-19 15:55:19 +0530

    Update 2set5.c

--- 2set5.c
+++ 2set5.c
@@ -1,3 +1,15 @@
 #include<stdio.h>
 int main()
 {
+  int min,max,i;
+  printf("enter the starting and ending number");
+  scanf("%d%d",&min,&max);
+  for(i=min+1;i<max;i++)
+  {
+    if(i%2==0)
+    {
+      printf("%d",i);
+    }
+  }
+  return 0;
+}
